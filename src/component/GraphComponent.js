8f30c48672e2231ad0fdd2bf4bce0348651e51cc
Szabolcs Mladoniczky <szabimlado@gmail.com>
2023-02-03 14:30:53 +0000

    created EventRegistration class that will be responsible for handling sigma events. made it so a new node is created when the user clicks on the graph.

--- src/component/GraphComponent.js
+++ src/component/GraphComponent.js
@@ -1,6 +1,5 @@
 import { useEffect } from "react";
-import { SigmaContainer, useLoadGraph } from "@react-sigma/core";
-import { Graph } from "graphology"
+import { SigmaContainer, useLoadGraph, useRegisterEvents, useSigma } from "@react-sigma/core";
 
 import "@react-sigma/core/lib/react-sigma.min.css";
 
@@ -8,19 +7,47 @@ function loadGraphEffect(graph, loadGraph) {
     loadGraph(graph)
 }
 
+class EventRegistration {
+    constructor(sigma, registerEvents) {
+        this.sigma = sigma
+        this.newNodeIndex = sigma.getGraph().order
+
+        registerEvents({
+            clickStage: this.clickStage.bind(this),
+            mousedown: this.mousedown.bind(this)
+        })
+    }
+
+    clickStage(event) {
+        const pos = this.sigma.viewportToGraph(event.event);
+        console.log(pos)
+        this.sigma.getGraph().addNode(this.newNodeIndex++, {x: pos.x, y: pos.y, color: "#000"})
+        event.event.preventSigmaDefault();
+        event.event.original.preventDefault();
+        event.event.original.stopPropagation();
+    }
+
+    mousedown() {
+        if (!this.sigma.getCustomBBox()) this.sigma.setCustomBBox(this.sigma.getBBox());
+    }
+}
+
 function GraphComponent({ graph }) {
 
-    function LoadGraph () {
+    function InitGraph () {
         const loadGraph = useLoadGraph();
+        const registerEvents = useRegisterEvents();
+        const sigma = useSigma()
       
         useEffect(() => {loadGraphEffect(graph, loadGraph)}, [loadGraph]);
+        useEffect(() => {new EventRegistration(sigma, registerEvents)}, [registerEvents, sigma]);
       
         return null;
       };
 
     return (
         <SigmaContainer settings={{ drawEdges: true, clone: false }}>
-            <LoadGraph />
+            <InitGraph />
         </SigmaContainer>
     )
 }
