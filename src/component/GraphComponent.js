0a203c3fa9c259830e203490f47675635bc7165a
Szabolcs Mladoniczky <szabimlado@gmail.com>
2023-02-03 14:29:22 +0000

    made the graph component use the supplied graph.

--- src/component/GraphComponent.js
+++ src/component/GraphComponent.js
@@ -4,31 +4,22 @@ import { Graph } from "graphology"
 
 import "@react-sigma/core/lib/react-sigma.min.css";
 
-function GraphComponent() {
+function loadGraphEffect(graph, loadGraph) {
+    loadGraph(graph)
+}
+
+function GraphComponent({ graph }) {
 
     function LoadGraph () {
         const loadGraph = useLoadGraph();
       
-        useEffect(() => {
-            const graph = new Graph();
-            graph.addNode("n1", { x: 0, y: 0, size: 10, color: "#aaa" });
-            graph.addNode("n2", { x: -5, y: 5, size: 10, color: "#bbb" });
-            graph.addNode("n3", { x: 5, y: 5, size: 10, color: "#ccc" });
-            graph.addNode("n4", { x: 0, y: 10, size: 10, color: "#ddd" });
-            graph.addEdge("n1", "n2");
-            graph.addEdge("n2", "n4");
-            graph.addEdge("n4", "n3");
-            graph.addEdge("n3", "n1");
-            loadGraph(graph);
-        }, [loadGraph]);
+        useEffect(() => {loadGraphEffect(graph, loadGraph)}, [loadGraph]);
       
         return null;
       };
 
-    const graph = new Graph();
-
     return (
-        <SigmaContainer graph={graph} settings={{ drawEdges: true, clone: false }}>
+        <SigmaContainer settings={{ drawEdges: true, clone: false }}>
             <LoadGraph />
         </SigmaContainer>
     )
