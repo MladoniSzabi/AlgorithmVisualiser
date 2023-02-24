d4729e4913deefe5b9b4a45744f8f1c559476895
Szabolcs Mladoniczky <szabimlado@gmail.com>
2023-02-24 15:07:44 +0000

    added search bar to the browse page.

--- src/pages/BrowsePage.js
+++ src/pages/BrowsePage.js
@@ -7,28 +7,41 @@ import "./BrowsePage.css"
 function BrowsePage() {
 
     let [graphList, setGraphList] = useState(null)
+    let [searchBarText, setSearchBarText] = useState("")
     useEffect(() => {
         getGraphList().then((graphList) => { setGraphList(graphList) })
     })
+
+    function onSearchBarChange(event) {
+        setSearchBarText(event.target.value)
+    }
 
     if (!graphList) {
         return <p>Loading...</p>
     }
 
     return (
-        <div id="graph-list">
-            {
-                Object.entries(graphList).map(
-                    ([key, value]) =>
-                        <div key={key} className="graph">
-                            <Link to={key}>
-                                <img src="graph-icon.svg" alt={"graph-icon"}></img>
-                                <p>{key}</p>
-                            </Link>
-                        </div>
-                )
-            }
-        </div>
+        <>
+            <div id="filters">
+                <p><label htmlFor="alg-search">Search:</label> <input id="alg-search" type="search" value={searchBarText} list="algs-datalist" onChange={onSearchBarChange}></input></p>
+            </div>
+            <datalist id="algs-datalist">
+                {Object.entries(graphList).map(([key, value]) => <option value={key} key={key}>{key}</option>)}
+            </datalist>
+            <div id="graph-list">
+                {
+                    Object.entries(graphList).filter(([key, value]) => (key.includes(searchBarText))).map(
+                        ([key, value]) =>
+                            <div key={key} className="graph">
+                                <Link to={key}>
+                                    <img src="graph-icon.svg" alt={"graph-icon"}></img>
+                                    <p>{key}</p>
+                                </Link>
+                            </div>
+                    )
+                }
+            </div>
+        </>
     )
 }
 
