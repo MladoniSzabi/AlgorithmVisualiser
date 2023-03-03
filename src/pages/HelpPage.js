5b389150f68b53cf7aee0d8ee7566c572a4c9898
Szabolcs Mladoniczky <szabimlado@gmail.com>
2023-03-03 13:21:15 +0000

    added skeleton of help page.

--- src/pages/HelpPage.js
+++ src/pages/HelpPage.js
@@ -1,3 +1,58 @@
 export default function HelpPage() {
-    return <h1>This is a help page</h1>
+    return <div>
+        <h1>Help</h1>
+        <div id="table-of-content">
+            <h3>Table of Content</h3>
+            <ul>
+                <li>Table of Content</li>
+            </ul>
+        </div>
+
+        <div id="graph-interactivity">
+            <h3>Interacting with the Graph</h3>
+            <div id="control">
+                <h5>Moving Around</h5>
+            </div>
+            <div id="add-vertex">
+                <h5>Adding a New Vertex</h5>
+            </div>
+            <div id="delete-vertex">
+                <h5>Deleting a Vertex</h5>
+            </div>
+            <div id="attributes">
+                <h5>Viewing and Modifying Attributes</h5>
+            </div>
+            <div id="add-attributes">
+                <h5>Adding New Attributes</h5>
+            </div>
+            <div id="delete-attributes">
+                <h5>Deleting Attributes</h5>
+            </div>
+        </div>
+
+        <div id="algorithm-about">
+            <h3>Your Algorithm</h3>
+            <div id="tech-stack">
+                <h5>Underlying Technology</h5>
+            </div>
+            <div id="algorithm-run">
+                <h5>Running Your Code</h5>
+            </div>
+            <div id="algorithm-history">
+                <h5>How Markers are Created</h5>
+            </div>
+            <div id="algorithm-history-group">
+                <h5>How to Group Markers</h5>
+            </div>
+            <div id="algorithm-reset">
+                <h5>The Reset Button</h5>
+            </div>
+            <div id="algorithm-new">
+                <h5>Creating a New Algorithm</h5>
+            </div>
+            <div id="algorithm-storage">
+                <h5>How Algorithms Are Stored</h5>
+            </div>
+        </div>
+    </div>
 }
